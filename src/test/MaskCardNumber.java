dc7285a0097174f4e080d89ac42b1defbb70b53d
Dipak Pawar <dbpawar877@gmail.com>
2021-10-11 18:43:48 +0530

    initila commit

---
+++ src/test/MaskCardNumber.java
@@ -0,0 +1,23 @@
+package test;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MaskCardNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome....!!!!!!"); 
+		System.out.println("Enter the Number"); 
+		Scanner sc = new Scanner(System.in);
+		String number  = sc.nextLine();
+		if (number.length()==8 && StringUtils.isNumeric(number)) {			     
+	    StringBuilder sb  = new StringBuilder();
+	    System.out.println("Mask Card Number = "+sb.append(number.substring(0,2)).append("XXXX").append(number.substring(6,8)));
+		}else {
+			System.out.println("You enter wrong number Could you please enter right number");
+			System.out.println("Prorper number tak kelay.!!!!!!!");
+		}
+	    
+	}
+
+}
---
+++ src/test/PallidromString.java
@@ -0,0 +1,25 @@
+package test;
+
+public class PallidromString {
+
+	public static void main(String[] args) {
+		String s1 = "madam";
+	    int count = 0 ;
+	    int j  = s1.length()-1;
+	    System.out.println("s1.length()-1 = "+j);
+	    for(int i =0 ; i<s1.length();i++) {
+	    	if(s1.charAt(i) == s1.charAt(j--)){
+	    		count++;
+	    	}	    	
+	    }
+	    System.out.println("count = "+count);
+	    System.out.println("s1.length() = "+s1.length());
+	    
+	    if(count == s1.length()) {
+	    	System.out.println("String is pallidrom");
+	    }else {
+	    	System.out.println("string is not pallidrom");
+	    }
+	    	
+	}
+}
